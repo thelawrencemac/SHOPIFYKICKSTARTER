a530c20cc37bb4c4010505ab609b288ebdc6d1cb
thelawrencemac <93192643+thelawrencemac@users.noreply.github.com>
2025-07-24 19:24:32 -0500

    k

--- api/index.js
+++ api/index.js
@@ -1,5 +1,12 @@
 export default function handler(req, res) {
-  res
-    .status(200)
-    .send("Shopify Kickstarter App is running. No frontend installed.");
+  const { shop } = req.query;
+  if (shop) {
+    // Start OAuth flow
+    res.writeHead(302, { Location: `/api/auth?shop=${shop}` });
+    res.end();
+  } else {
+    res
+      .status(200)
+      .send("Shopify Kickstarter App is running. No frontend installed.");
+  }
 }
